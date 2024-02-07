4684443d216d72c632ef44058dede8df1f75ead5
sasha <sasha2323002@bk.ru>
2024-02-07 22:01:34 +0300

    end

--- src/processing/processing.module.ts
+++ src/processing/processing.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ProcessingService } from './processing.service';
-import { ProcessingController } from './processing.controller';
 import { CsvService } from 'src/csv/csv.service';
 import { HttpCommunicationService } from 'src/http-communication/http-communication.service';
 import { OrderService } from 'src/order/order.service';
@@ -8,7 +7,6 @@ import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
-  controllers: [ProcessingController],
   providers: [
     ProcessingService,
     HttpCommunicationService,
